3ab2aa0c5e76c1e087e6390325eae250cb70fdab
tatelucas <tate.lucas@gmail.com>
2015-02-22 22:20:33 -0500

    theme setup and homepage content

--- wp-content/themes/mayan/templates/head.php
+++ wp-content/themes/mayan/templates/head.php
@@ -8,5 +8,20 @@
 
   <?php wp_head(); ?>
 
+  <script src="//use.typekit.net/htt8oox.js"></script>
+  <script>try{Typekit.load();}catch(e){}</script>
+
+  <link rel="apple-touch-icon" sizes="57x57" href="/wp-content/themes/mayan/assets/img/favicon/apple-touch-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="/wp-content/themes/mayan/assets/img/favicon/apple-touch-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="/wp-content/themes/mayan/assets/img/favicon/apple-touch-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="/wp-content/themes/mayan/assets/img/favicon/apple-touch-icon-76x76.png">
+  <link rel="icon" type="image/png" href="/wp-content/themes/mayan/assets/img/favicon/favicon-32x32.png" sizes="32x32">
+  <link rel="icon" type="image/png" href="/wp-content/themes/mayan/assets/img/favicon/favicon-16x16.png" sizes="16x16">
+  <link rel="manifest" href="/wp-content/themes/mayan/assets/img/favicon/manifest.json">
+  <link rel="shortcut icon" href="/wp-content/themes/mayan/assets/img/favicon/favicon.ico">
+  <meta name="msapplication-TileColor" content="#da532c">
+  <meta name="msapplication-config" content="/wp-content/themes/mayan/assets/img/favicon/browserconfig.xml">
+  <meta name="theme-color" content="#ffffff">
+
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
 </head>
